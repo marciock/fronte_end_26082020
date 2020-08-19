2cc5fed5d185a84881d4d8399efbb500d2777f71
marciock <marciockalves@gmail.com>
2020-08-19 16:57:10 -0300

    first commit

---
+++ js/produtos.js
@@ -0,0 +1,18 @@
+const data=[
+    {
+        produto:'Pera',
+        preco:'2.30'
+    },
+    {
+        produto:'Mamão',
+        preco:'4.50'
+    },
+    {
+        produto:'Jaca',
+        preco:'4.00'
+    },
+    {
+        produto:'Melnacia',
+        preco:'6.00'
+    }
+]
